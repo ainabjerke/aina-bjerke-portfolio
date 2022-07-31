97213bb3f3f77ea1b63d1ce20636ce74f0f734d1
ainabjerke <83291997+ainabjerke@users.noreply.github.com>
2022-07-31 13:19:41 +0200

    Added new project
    home decor

--- js/modal.js
+++ js/modal.js
@@ -3,18 +3,21 @@ var modalOne = document.getElementById("myModalOne");
 var modalTwo = document.getElementById("myModalTwo");
 var modalThree = document.getElementById("myModalThree");
 var modalFour = document.getElementById("myModalFour");
+var modalFive = document.getElementById("myModalFive");
 // Get the button that opens the modal
 // var btn = document.getElementById("myBtn");
 var btnOne = document.getElementById("myBtnOne");
 var btnTwo = document.getElementById("myBtnTwo");
 var btnThree = document.getElementById("myBtnThree");
 var btnFour = document.getElementById("myBtnFour");
+var btnFive = document.getElementById("myBtnFive");
 
 // Get the <span> element that closes the modal
 var spanOne = document.getElementsByClassName("closeOne")[0];
 var spanTwo = document.getElementsByClassName("closeTwo")[0];
 var spanThree = document.getElementsByClassName("closeThree")[0];
 var spanFour = document.getElementsByClassName("closeFour")[0];
+var spanFive = document.getElementsByClassName("closeFive")[0];
 // When the user clicks the button, open the modal
 // btn.onclick = function () {
 //   modal.style.display = "block";
@@ -35,6 +38,10 @@ btnFour.onclick = function () {
   modalFour.style.display = "block";
 };
 
+btnFive.onclick = function () {
+  modalFive.style.display = "block";
+};
+
 // When the user clicks on <span> (x), close the modal
 spanOne.onclick = function () {
   modalOne.style.display = "none";
@@ -50,6 +57,10 @@ spanThree.onclick = function () {
 
 spanFour.onclick = function () {
   modalFour.style.display = "none";
+};
+
+spanFive.onclick = function () {
+  modalFive.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
@@ -68,6 +79,8 @@ window.onclick = function (event) {
     modalThree.style.display = "none";
   } else if (event.target == modalFour) {
     modalFour.style.display = "none";
+  } else if (event.target == modalFive) {
+    modalFive.style.display = "none";
   }
 };
 
